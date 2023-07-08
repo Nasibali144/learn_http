f0d73dabb31441f4a17b805dc50fd1936d6f6bec
Nasibali144 <abdiyevnasibali@gmail.com>
2023-07-08 18:27:52 +0500

    Movie App: commit 4: DetailPage completed

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -1,42 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:learn_http/models/movie.dart';
+import 'package:learn_http/pages/error_page.dart';
+import 'package:learn_http/pages/loading_page.dart';
 import 'package:learn_http/services/network_service.dart';
 
-class DetailPage extends StatefulWidget {
+class DetailPage extends StatelessWidget {
   final int id;
+
   const DetailPage({Key? key, required this.id}) : super(key: key);
 
   @override
-  State<DetailPage> createState() => _DetailPageState();
+  Widget build(BuildContext context) {
+    return FutureBuilder<String?>(
+      future: Network.methodGet(api: Network.apiMovies, id: id),
+      initialData: "No Data",
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none: return const ErrorPage(message: "No Data");
+          case ConnectionState.waiting: return const LoadingPage();
+          done:
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              return DetailSuccessPage(data: snapshot.data!);
+            } else {
+              return const ErrorPage(
+                  message:
+                      "Something error, Please check your internet connecting!");
+            }
+          case ConnectionState.active: continue done;
+        }
+      },
+    );
+  }
 }
 
-class _DetailPageState extends State<DetailPage> {
-  Movie? movie;
+class DetailSuccessPage extends StatelessWidget {
+  final String data;
 
-  @override
-  void initState() {
-    super.initState();
-    fetchMovie(widget.id);
-  }
-
-  void fetchMovie(int id) async {
-    final data = await Network.methodGet(api: Network.apiMovies, id: id);
-    movie = Network.parseMovie(data!);
-    setState(() {});
-  }
+  const DetailSuccessPage({Key? key, required this.data}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return movie != null ? Scaffold(
+    final Movie movie = Network.parseMovie(data);
+
+    return Scaffold(
       appBar: AppBar(
-        title: Text(movie!.movie),
+        title: Text(movie.movie),
       ),
       body: Container(
         margin: const EdgeInsets.all(20),
-      ),
-    ) : const Scaffold(
-      body: Center(
-        child: CircularProgressIndicator.adaptive(),
+        alignment: Alignment.center,
+        child: Text(
+          "$movie",
+          style: Theme.of(context).textTheme.headlineMedium,
+        ),
       ),
     );
   }
